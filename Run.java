e9590072b11284d1f52cb1ef3cc7de2d104ad8e9
Pratyush Kumar <66841352+pratyushgta@users.noreply.github.com>
2020-08-17 21:18:02 +0530

    RUN Game
    This is the main class of the game. Click on run on this class to play the game. Since the game is still in development, you can test the individual features by checking out the project.

--- Run.java
+++ Run.java
@@ -1,5 +1,6 @@
 /**
  * This is the main class of the game. Click on run on this class to play the game. 
+ *Since the game is still in development, you can test the individual features by checking out the project. 
  */
 
 public class Run {
