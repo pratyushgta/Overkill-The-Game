54c61296c8a82c6d09ab1688475c43933e4a7db2
Pratyush Kumar <pratyushgta@gmail.com>
2020-09-06 02:18:06 +0530

    September Update 1 | City Metro now Up & Running!

--- RunTemp.java
+++ RunTemp.java
@@ -2,8 +2,13 @@ import UI.*;
 
 import java.io.IOException;
 
+/**
+ * Main class of the game.  
+ * Run this class only if you want to experience different parts of city.
+ * OR, click on Run class if you want to quickly test various features of the game.
+ */
 public class RunTemp {
     public static void main(String[] args) throws IOException, InterruptedException {
-        mainMenu.main(new String[]{});
+        TempMainMenu.main(new String[]{});
     }
 }
